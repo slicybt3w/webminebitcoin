abdafc3c4eb1916c431d7145f00aadbcbf740b9d
slicybt3w <80214266+slicybt3w@users.noreply.github.com>
2021-09-19 13:20:47 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 echo "made by slicybtw";
 echo "join the discord server: https://discord.gg/8M9fhmeUzM";
 echo "follow on github: https://github.com/slicybt3w";
-echo "subsicribe on YouTube: ";
+echo "subsicribe on YouTube: https://youtube.com/channel/UCqwE6vaLW1eVX6L3msbvTzw";
 yellow = "\033[1;33m"
 green = "\034[0;32m"
 #installing php
