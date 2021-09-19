aa331571c92b0de5af370bc673c2d2864a9b117a
slicybt3w <80214266+slicybt3w@users.noreply.github.com>
2021-09-19 12:33:29 +0300

    Add files via upload

---
+++ user/login/login.php
@@ -0,0 +1,42 @@
+<?php 
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<meta property="og:description" content="login to web mine bitcoin">
+<meta property="og:title" content="web mine Bitcoin login">
+
+</head>
+
+<body>
+<div id="login">
+<form action="login.php" method="POST">
+
+<p>
+<label>Username:</label>
+</p>
+<input type="username" id="user" name="user" placeholder="username" required>
+
+<p>
+<label>Email:</label>
+</p>
+<input type="email" id="email" name="email" placeholder="email" required>
+
+<p>
+<label>password:</label>
+</p>
+<input type="password" id="password" name="password" placeholder="password" required>
+
+<p>
+<input type="submit" value="login" id="btn" name="btn">
+</p>
+</form>
+</div>
+
+
+</body>
+
+</html>
