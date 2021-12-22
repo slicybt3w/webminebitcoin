76f55f78a6c2e83e0d23f0550645dbff8e3b9609
slicybt3w <80214266+slicybt3w@users.noreply.github.com>
2021-12-22 16:09:23 +0300

    Update process.php

--- user/process.php
+++ user/process.php
@@ -5,10 +5,10 @@ include "config.php";
 
 error_reporting(0);
 $username = $_POST['name'];
-$password = $_POST['pass'];                                                                                                                                                     //prevent mysql injection                                                               $username = stripslashes($username);                                                    $password = stripslashes($password);
+$password = $_POST['pass']; 
+//prevent mysql injection                                                               $username = stripslashes($username);                                                    $password = stripslashes($password);
 $con = mysqli_connect($host, $username, $password, "login");                            
-$result = mysqli_query($con,"select * from login where  username= '$username' and passwo
-rd = '$password'")
+$result = mysqli_query($con,"select * from login where  username= '$username' and password = '$password'")
         or die("couldnt query database".mysql_error());
 
 $row = mysqli_fetch_array($result);
